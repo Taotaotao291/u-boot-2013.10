874167cac7c89f7840c9a1401963dfdc75837822
Taotaotao <taotaotao291@163.com>
2018-06-26 21:15:48 -0700

    mk.sh: Add project build script.

---
+++ mk.sh
@@ -0,0 +1,7 @@
+
+cross_compile=/home/taotaotao/linux/arm-linux-gcc/arm-2009q3/bin/arm-none-linux-gnueabi-
+arch=arm
+
+make ARCH=${arch} CROSS_COMPILE=${cross_compile} distclean
+make ARCH=${arch} CROSS_COMPILE=${cross_compile} s5p_goni_config
+make ARCH=${arch} CROSS_COMPILE=${cross_compile} all -j4
